c6da5fc90501f951f3e1f7c19b8408bff126ac85
Lindternal <den_080202@mail.ru>
2024-01-25 09:49:59 +0300

    bugfix

--- Constants.cs
+++ Constants.cs
@@ -1,5 +1,5 @@
 ﻿public class Constants
 {
-    public const string DatabaseFile = @"..\..\..\db.sqlite";
+    public const string DatabaseFile = "db.sqlite";
     public const string DatabaseConnectionString = "Data Source=" + DatabaseFile;
 }
